360b84aaa9ca6156729902628af4e2fa5b3c1d89
Shankar Morwal <morwal89@gmail.com>
2016-08-14 14:22:37 +0530

    first commit

---
+++ packages/simple/README.md
@@ -0,0 +1,1 @@
+This is simple package for using meteor.js
---
+++ packages/simple/package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: 'morwal89:simple',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.4.0.1');
+  api.use('ecmascript');
+  api.mainModule('simple.js');
+  api.use("templating", "client");
+  api.addFiles("simple-template.html", "client");
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('morwal89:simple');
+  api.mainModule('simple-tests.js');
+});
