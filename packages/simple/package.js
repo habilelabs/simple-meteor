fc0f539267c9bffebfc839af9e15415f401b2cff
Shankar Morwal <morwal89@gmail.com>
2016-08-14 14:52:33 +0530

    read me

--- packages/simple/package.js
+++ packages/simple/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'morwal89:simple',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'This is sample meteor package for tutorial',
   // URL to the Git repository containing the source code for this package.
